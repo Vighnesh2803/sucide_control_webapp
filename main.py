794af5b18d165eaf62cb677181537651cec28b33
Vighnesh Sawant <youremail@example.com>
2025-08-08 09:21:39 +0530

    my first get rrequest

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI!"}
+
+
+
+@app.get("/my")
+def read_root():
+    return {"message": "Hello from vighnesh!"}
+
+
+
+# RUN THIS FILE
+#  uvicorn app.main:app --reload
